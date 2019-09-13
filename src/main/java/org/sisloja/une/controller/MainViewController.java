89d1f452279bf5a24def0546ec305892cc71f633
aquilarafa <rafaelaquilaah@hotmail.com>
2019-09-13 00:47:00 -0300

    iniciando mudanças de views pelos botoes

--- src/main/java/org/sisloja/une/controller/MainViewController.java
+++ src/main/java/org/sisloja/une/controller/MainViewController.java
@@ -14,61 +14,91 @@ import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 
-public class MainViewController implements Initializable{
+public class MainViewController implements Initializable {
 
-    @FXML
-    private JFXDrawer drawer;
+	@FXML
+	private AnchorPane anchorPane;
 
-    @FXML
-    private JFXHamburger hamburger;
+
+	@FXML
+	private JFXDrawer drawer;
+
+	@FXML
+	private JFXHamburger hamburger;
+
+	private SideBarController sideBar;
 
 	public void initialize(URL location, ResourceBundle resources) {
-		
-		
-		VBox box = (VBox) loadNode("/fxml/SideBar.fxml");
+
+		// loads and sets SideBar to the drawer
+		Parent node = null;
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/SideBar.fxml"));
+			node = loader.load();
+			sideBar = loader.getController();
+			// warns side bar controller to pick this main view
+			sideBar.init(this);
+
+		} catch (IOException ex) {
+			Alerts.showAlert("IO Exception", "Error loading view", ex.getMessage(), AlertType.ERROR);
+		}
+		VBox box = (VBox) node;
 		drawer.setSidePane(box);
-		
-		//sidebar nodes
-		for(Node node: box.getChildren()) {
+
+		//
+		final HamburgerBackArrowBasicTransition transition = new HamburgerBackArrowBasicTransition(hamburger);
+		transition.setRate(-1);
+		hamburger.addEventHandler(MouseEvent.MOUSE_PRESSED, new EventHandler<MouseEvent>() {
+			public void handle(MouseEvent e) {
+				transition.setRate(transition.getRate() * -1);
+				transition.play();
+
+				if (drawer.isOpened()) {
+					drawer.close();
+				} else {
+					drawer.open();
+				}
+			}
+		});
+
+	}
+
+	public void changeMainPane(String sourceId) {
+
+		if(sourceId.equals("btnDepartamentos")) {
+			//mainPane.setCenter(load("/fxml/DepartamentosView.fxml"));
+			AnchorPane pane = (AnchorPane) load("/fxml/DepartamentosView.fxml");
+			anchorPane.getChildren().setAll(pane);
 			
 		}
 		
-		//
-		final HamburgerBackArrowBasicTransition transition = new HamburgerBackArrowBasicTransition(hamburger);
-        transition.setRate(-1);
-        hamburger.addEventHandler(MouseEvent.MOUSE_PRESSED, new EventHandler<MouseEvent>() {
-			public void handle(MouseEvent e) {
-			    transition.setRate(transition.getRate() * -1);
-			    transition.play();
-
-			    if (drawer.isOpened()) {
-			        drawer.close();
-			    } else {
-			        drawer.open();
-			    }
-			}
-		});
-		
 	}
 	
-	private Parent loadNode(String absolutePath) {
+	private Parent load(String location) {
 		Parent node = null;
 		try {
-            FXMLLoader loader = new FXMLLoader(getClass().getResource(absolutePath));
-            node = loader.load();
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(location));
+			node = loader.load();
 
-        } catch (IOException ex) {
-        	Alerts.showAlert("IO Exception", "Error loading view", ex.getMessage(), AlertType.ERROR);
-        }
+		} catch (IOException ex) {
+			Alerts.showAlert("IO Exception", "Error loading view", ex.getMessage(), AlertType.ERROR);
+			System.out.println(ex.getMessage());
+			ex.printStackTrace();
+			
+		}
 		
 		return node;
 	}
 
-}
 
+
+
+
+}
---
+++ src/main/java/org/sisloja/une/controller/SideBarController.java
@@ -0,0 +1,59 @@
+package org.sisloja.une.controller;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import com.jfoenix.controls.JFXButton;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Tab;
+import javafx.scene.layout.AnchorPane;
+
+public class SideBarController implements Initializable{
+	
+	@FXML
+	private JFXButton btnVendas;
+	
+	@FXML 
+	private JFXButton btnVendedores;
+
+	@FXML 
+	private JFXButton btnProdutos;
+
+	@FXML 
+	private JFXButton btnDepartamentos;
+
+	@FXML 
+	private JFXButton btnRelatorios;
+	
+	@FXML
+	private JFXButton btnSair;
+	
+
+	private MainViewController myMainViewController;
+
+	
+
+	public void initialize(URL location, ResourceBundle resources) {
+		
+		
+	}
+	
+	@FXML
+	private void changeView(ActionEvent e) {
+		JFXButton whoGenerated = (JFXButton) e.getSource();
+		myMainViewController.changeMainPane(whoGenerated.getId());
+	}
+
+
+
+	public void init(MainViewController mainViewController) {
+		myMainViewController = mainViewController;
+		
+	}
+
+}
